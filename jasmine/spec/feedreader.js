1315faf389027b8a65858b333c4cd22981b0f37a
Eugene B <enbotnaru@gmail.com>
2018-06-22 14:45:24 -0700

    Add object name and menu tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -35,32 +35,52 @@ $(
         for (let i = 0; i < allFeeds.length; i++) {
           expect(allFeeds[i].hasOwnProperty("url")).toBe(true);
           if (allFeeds[i].hasOwnProperty("url")) {
-            expect(allFeeds[i]["url"]).not.toBe(null);
-            expect(allFeeds[i]["url"]).not.toBe("");
+            // test if url value is not empty
+            expect(allFeeds[i]["url"].length).not.toBe(0);
           }
         }
       });
-
       /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+       it("has name defined and is not empty", function() {
+         for (let i = 0; i < allFeeds.length; i++) {
+           expect(allFeeds[i].hasOwnProperty("name")).toBe(true);
+           if (allFeeds[i].hasOwnProperty("name")) {
+             // test if url value is not empty
+             expect(allFeeds[i]["name"].length).not.toBe(0);
+           }
+         }
+       });
     });
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe("The menu", function () {
     /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+         it('is hidden by default', function () {
+           expect(document.body.className).toBe("menu-hidden");
+         })
     /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+          it("changes visibility when clicked", function () {
+            let menu = document.body.className;
+            let menuIcon = document.querySelector('.menu-icon-link');
+            menuIcon.onclick = function (event) {
+              return true;
+            }
+            if (menuIcon.onclick) {
+            expect(menu === "menu-hidden" || menu === "").toBe(true)
+          }
+          })
+      });
     /* TODO: Write a new test suite named "Initial Entries" */
 
     /* TODO: Write a test that ensures when the loadFeed
